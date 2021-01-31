1dca27ee9a72668abc6a7b4bcb9bbdbca2b31286
paone6 <57143762+paone6@users.noreply.github.com>
2021-01-31 13:27:24 +0100

    prima versone my_dataset e load_my_dataset

--- my_dataset.py
+++ my_dataset.py
@@ -2,6 +2,29 @@ import numpy as np
 import csv
 from traitlets.utils.bunch import Bunch
 import pandas as pd
+import os
+
+
+path = "/Users/paone/Desktop/csv_computati"  #Path della cartella contenente il dataset
+
+def load_my_dataset():
+    dataset = []
+    targets = []
+    for file in os.listdir(path):     #per ogni file video nella cartella
+        matrix = []
+        with open(path + "/" + file) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                new_row = [float(i) for i in row]
+                matrix.append(new_row)
+            targets.append(file.split('_')[0])
+            dataset.append(matrix)
+    return Bunch(data = dataset, target = targets)
+
+b = load_my_dataset()
+print(b.data)
+print(b.target)
+
 
 
 
