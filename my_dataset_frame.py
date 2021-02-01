fc97b1ba0fe848895cb69178cf917a16a6908e83
paone6 <57143762+paone6@users.noreply.github.com>
2021-02-01 22:13:15 +0100

    nuovo my_database_frame e prove su classificatore

--- my_dataset_frame.py
+++ my_dataset_frame.py
@@ -11,19 +11,17 @@ def load_my_dataset():
     dataset = []
     targets = []
     for file in os.listdir(path):     #per ogni file video nella cartella
-        matrix = []
         with open(path + "/" + file) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
             for row in csv_reader:
                 new_row = [float(i) for i in row]
                 np_new_row = np.array(new_row)
-                matrix.append(np_new_row)                
-            targets.append(int(file.split('_')[0]))
-            np_matrix = np.array(matrix)
-            dataset.append(np_matrix)
+                dataset.append(np_new_row)               
+                targets.append(int(file.split('_')[0]))
     np_targets = np.array(targets)
     np_dataset = np.array(dataset, dtype=object)
     return Bunch(data = np_dataset, target = np_targets)
+
 
 
 
